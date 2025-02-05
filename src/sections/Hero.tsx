62f018cc393c87932524ad780ec93dae88ac0fe8
harshbhati1 <harshbhati10216@gmail.com>
2025-02-05 12:34:42 -0500

    updated-resune

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -79,7 +79,7 @@ export const HeroSection = () => {
       </div>
       <div className='flex flex-col md:flex-row justify-center items-center mt-8 gap-4 relative z-50'>
       <Link
-    href="https://drive.google.com/file/d/1g41n2ef-dqriFsYio75WGrRP0LqfvXHU/view?usp=sharing"
+    href="https://drive.google.com/file/d/1zDlvRuqFnGwePG1aXgEt3hrZ6CnSXjYa/view?usp=sharing"
     target="_blank"
     rel="noopener noreferrer"
     className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl"
